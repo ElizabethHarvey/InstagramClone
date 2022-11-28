c6da9e2f060e178a52b7115b1605341579b7b2bf
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-28 15:05:57 -0500

    Added the DiscoverScreen

---
+++ screens/DiscoverScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function DiscoverScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Text>Hey</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default DiscoverScreen;
