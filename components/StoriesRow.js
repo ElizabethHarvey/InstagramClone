17e041e499c765d343b8c33da4278de6e88427db
Lizzy Harvey <lizzy220188@gmail.com>
2022-12-05 16:21:07 -0500

    Styling for dummy data in StoriesList

---
+++ components/StoriesRow.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const StoriesRow = ({ image, name, lock }) => {
+  return (
+    <View>
+      <TouchableOpacity style={styles.row}>
+        <View style={styles.profile}>
+          <Image style={styles.image} source={{ uri: image }} />
+        </View>
+        <Ionicons style={styles.lock} name={lock} size='16' color='#AD4EC9' />
+        <Text style={styles.name}>{name}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    alignItems: 'center',
+    height: 80,
+    marginBottom: 35,
+    padding: '1%',
+    left: '10%',
+    top: 10,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: '100%',
+  },
+  name: {
+    top: '115%',
+    fontSize: 15,
+    position: 'absolute',
+  },
+  profile: {
+    top: '20%',
+    width: 70,
+    height: 70,
+    borderRadius: '100%',
+    borderColor: '#AD4EC9',
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lock: {
+    backgroundColor: '#E7E6E6',
+  },
+});
+
+export default StoriesRow;
