2c908c5ada573e24973370f2f386ead1f5e2727c
Lizzy Harvey <lizzy220188@gmail.com>
2022-12-05 16:21:46 -0500

    Finished the header and added more icons to the HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,20 +8,33 @@ import {
   View,
 } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import StoriesList from '../components/StoriesList';
 
 function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ left: 20, top: 10 }}>
-        <Image
-          source={require('../assets/instagramText.png')}
-          style={styles.instagram}
-        />
-        <TouchableOpacity style={{ flexDirection: 'row',}}>
-        <Ionicons style={{ left: 17,}} name='add-outline' size={15}/>
-        <Ionicons name='square-outline' size={20}/>
-        </TouchableOpacity>
+      <View style={{ top: 10 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
+          <Image
+            source={require('../assets/instagramText.png')}
+            style={styles.instagram}
+          />
+          <TouchableOpacity style={styles.postIcon}>
+            <Ionicons
+              style={{ borderWidth: 2, borderRadius: '10%' }}
+              name='add'
+              size={20}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.heart}>
+            <Ionicons name='heart-outline' size={30} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.chat}>
+            <Ionicons name='chatbox-outline' size={30} />
+          </TouchableOpacity>
+        </View>
       </View>
+      <StoriesList />
     </SafeAreaView>
   );
 }
@@ -31,9 +44,19 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   instagram: {
-    width: '35%',
-    height: '27%',
+    width: 120,
+    height: 35,
     resizeMode: 'container',
+    left: 10,
+  },
+  postIcon: {
+    left: '290%',
+  },
+  heart: {
+    left: '140%'
+  },
+  chat: {
+    right: 10,
   },
 });
 
