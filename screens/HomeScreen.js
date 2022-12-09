63420676afc385b95d4db84eaf8a1ecf1da3f72d
Lizzy Harvey <lizzy220188@gmail.com>
2022-12-09 11:25:06 -0500

    Added more icons and added a image to  the homeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Image,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -14,12 +15,24 @@ import FypList from '../components/FypList';
 function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ top: 10 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
+      <View style={{ top: 10, marginBottom: '3%' }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
           <Image
             source={require('../assets/instagramText.png')}
             style={styles.instagram}
           />
+           <TouchableOpacity style={{ right: '100%' }}>
+            <Ionicons
+              name='chevron-down'
+              size={20}
+            />
+          </TouchableOpacity>
           <TouchableOpacity style={styles.postIcon}>
             <Ionicons
               style={{ borderWidth: 2, borderRadius: '10%' }}
@@ -30,13 +43,15 @@ function HomeScreen() {
           <TouchableOpacity style={styles.heart}>
             <Ionicons name='heart-outline' size={30} />
           </TouchableOpacity>
-          <TouchableOpacity  style={styles.chat}>
+          <TouchableOpacity style={styles.chat}>
             <Ionicons name='chatbox-outline' size={30} />
           </TouchableOpacity>
         </View>
       </View>
-      <StoriesList />
-      <FypList />
+      <ScrollView>
+        <StoriesList />
+        <FypList />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -52,10 +67,10 @@ const styles = StyleSheet.create({
     left: 10,
   },
   postIcon: {
-    left: '290%',
+    left: '200%',
   },
   heart: {
-    left: '140%'
+    left: '90%',
   },
   chat: {
     right: 10,
