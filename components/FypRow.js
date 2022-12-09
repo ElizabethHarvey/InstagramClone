375d99627d32c27d76f67fdae5cc23e996818f3e
Lizzy Harvey <lizzy220188@gmail.com>
2022-12-09 11:23:48 -0500

    Added more icons and styling to the Stories list

--- components/FypRow.js
+++ components/FypRow.js
@@ -3,18 +3,22 @@ import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 import LinearGradient from 'react-native-linear-gradient';
+import StoriesList from './StoriesList';
 
 const FypRow = ({ image, name, lock }) => {
   return (
     <View>
       <TouchableOpacity style={styles.row}>
-        <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <View style={styles.profile}>
             <Image style={styles.image} source={{ uri: image }} />
           </View>
           <Text style={styles.name}>{name}</Text>
         </View>
         {/* <Ionicons style={styles.lock} name={lock} size='16' color='#AD4EC9' /> */}
+        <TouchableOpacity style={styles.post}>
+          <Image style={{width: '95%', height: 500, resizeMode: 'container', }} source={{ uri: image }} />
+        </TouchableOpacity>
       </TouchableOpacity>
     </View>
   );
@@ -22,8 +26,8 @@ const FypRow = ({ image, name, lock }) => {
 
 const styles = StyleSheet.create({
   row: {
-    top: '20%',
-    marginBottom: '10%',
+    top: '5%',
+    marginBottom: '25%',
   },
   image: {
     width: 35,
@@ -32,8 +36,12 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 15,
+    bottom: '2%',
+    left: '70%',
   },
   profile: {
+    left: '3%',
+    bottom: '20%',
     width: 43,
     height: 43,
     borderRadius: '100%',
@@ -46,6 +54,9 @@ const styles = StyleSheet.create({
   lock: {
     backgroundColor: '#E7E6E6',
   },
+  post: {
+    alignItems: 'center',
+  }
 });
 
 export default FypRow;
